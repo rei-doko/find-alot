4c96dbe0935b04d0cb094a41919d7d22aa6abbaa
rei.doko <jr.danas23@gmail.com>
2026-03-02 09:00:44 +0800

    Added block

---
+++ src/MyLib/Block.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package MyLib;
+
+import java.util.List;
+
+/**
+ *
+ * @author rei doko
+ */
+public class Block {
+    private static int blockCounter = 0;
+    private int blockNumber;
+    private List<Property> properties;
+}
--- src/MyLib/Property.java
+++ src/MyLib/Property.java
@@ -8,6 +8,23 @@ package MyLib;
  *
  * @author rei doko
  */
-public class Property {
-    
+public abstract class Property {
+    private Block block;
+    private static int idCounter = 0;
+    private int propertyId;
+    private String propertyType;
+    private int floors = 2;
+    private String status = "For Sale";
+    private double propertySize;
+
+    Property(Block block, String propertyType, double propertySize) {
+        this.block = block;
+        this.propertyId = generateId();
+        this.propertyType = propertyType;
+        this.propertySize = propertySize;
+    }
+
+    private int generateId() {
+        return ++idCounter;
+    }
 }
