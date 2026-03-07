188f6c6a7ad1be18bb1449c6efce2eb4f480ea33
OkamiDLR <gcdelarosa06@gmail.com>
2026-03-07 15:00:11 +0800

    copy-paste of Cash, not yet finished

---
+++ src/MyLib/Mortgage.java
@@ -0,0 +1,64 @@
+public class Mortgage {
+    private Property property;
+
+    private double contactPrice;
+    private double capitalTax;
+    private double documentaryStampTax;
+    private double transferTax;
+    private double registrationFee;
+    private double notarial;
+    private double agentFee;
+
+    private int monthsToPay;
+    private double monthlyPayment;
+
+    private double finalPayment;
+
+    public Mortgage(Property property) {
+        this.property = property;
+        this.contactPrice = property.getContactPrice();
+
+        capitalTax=contactPrice*0.06;
+        documentaryStampTax=contactPrice*0.015;
+        transferTax=contactPrice*0.05;
+        registrationFee=contactPrice*0.03;
+        notarial=contactPrice*0.01;
+        agentFee=contactPrice*0.03;
+        
+        finalPayment=contactPrice+documentaryStampTax+transferTax+registrationFee+notarial;
+    }
+    
+    public void showReceipt(){
+        System.out.println("Contact Price:"+contactPrice);
+        System.out.println("Capital Tax: "+capitalTax);
+        System.out.println("Documentary Stamp Tax: "+documentaryStampTax);
+        System.out.println("Transfer Tax: "+transferTax);
+        System.out.println("Registration Fee: "+registrationFee);
+        System.out.println("Notarial Fee: "+notarial);
+        System.out.println("------------------------------");
+        System.out.println("Final Payment: "+finalPayment);
+    }
+
+    ////////////////////
+    private boolean agentFeeConfirmed=false;
+
+    public double getAgentFee() {
+        return agentFee;
+    }
+
+    public void setAgentFee(double agentFee) {
+        this.agentFee = agentFee;
+    }
+    /* CODE FOR ADMIN
+    public void confirmAgentFee(Payment agentPay) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(name + ", do you want to confirm the agent fee of " + agentPay.getAgentFee() + "? (yes/no)");
+
+        String response = scanner.nextLine().trim().toLowerCase();
+        if (response=="yes" || response=="y") {
+            System.out.println("Agent fee has been confirmed.");
+        } else {
+            System.out.println("Agent fee remains unconfirmed.");
+        }
+    } */
+}
