6bd8fcaf8d6d3fecf199e307c57567a071823520
OkamiDLR <gcdelarosa06@gmail.com>
2026-03-18 12:00:24 +0800

    finished fixing the attributes of the lots

--- src/MyLib/PropertyFactory.java
+++ src/MyLib/PropertyFactory.java
@@ -13,19 +13,96 @@ public class PropertyFactory {
     
     protected static Property createProperty(int blockNum, int lotNum) {
         type++;
-        int select = type % 3;
+        int select = type % 10;
 
         int floors = 2;
-        double size = 150.00;//placeholder-temporary
-        double price = 4000000.00;//placeholder-temporary
-
-        switch(select){
+        double price = 100.0;
+        double size = 100.0;
+        
+        switch(blockNum){
             case 1:
-                return new Detached(blockNum, lotNum, floors, size, price, 2, 50.0);
             case 2:
-                return new SemiDetached(blockNum, lotNum, floors, size, price, "Left", 25.0);
-            case 0:
-                return new TownHouse(blockNum, lotNum, floors, size, price, 2);
+                switch(select){
+                    case 1:
+                    case 2:
+                    case 3:
+                    case 4:
+                        floors = 2;
+                        price = 3400000.0;
+                        size = 60.0;
+                        return new TownHouse(blockNum, lotNum, floors, size, price, 3);
+                    case 5:
+                    case 6:
+                    case 7:
+                        floors = 2;
+                        price = 3800000.0;
+                        size = 75.0;
+                        return new TownHouse(blockNum, lotNum, floors, size, price, 3);
+                    case 8:
+                    case 9:
+                    case 0:
+                        floors = 3;
+                        price = 4000000.0;
+                        size = 80.0;
+                        return new TownHouse(blockNum, lotNum, floors, size, price, 3);
+                    default:
+                        return null;
+                }
+            case 3:
+            case 4:
+                switch(select){
+                    case 1:
+                    case 2:
+                    case 3:
+                    case 4:
+                        floors = 2;
+                        price = 4000000.0;
+                        size = 60.0;
+                        return new SemiDetached(blockNum, lotNum, floors, size, price, "Left", 50.0);
+                    case 5:
+                    case 6:
+                    case 7:
+                        floors = 2;
+                        price = 4200000.0;
+                        size = 75.0;
+                        return new SemiDetached(blockNum, lotNum, floors, size, price, "Left", 50.0);
+                    case 8:
+                    case 9:
+                    case 0:
+                        floors = 3;
+                        price = 4500000.0;
+                        size = 80.0;
+                        return new SemiDetached(blockNum, lotNum, floors, size, price, "Left", 50.0);
+                    default:
+                        return null;
+                }
+            case 5:
+                switch(select){
+                    case 1:
+                    case 2:
+                    case 3:
+                    case 4:
+                        floors = 2;
+                        price = 4300000.0;
+                        size = 60.0;
+                        return new Detached(blockNum, lotNum, floors, size, price, 2, 50.0);
+                    case 5:
+                    case 6:
+                    case 7:
+                        floors = 2;
+                        price = 4500000.0;
+                        size = 75.0;
+                        return new Detached(blockNum, lotNum, floors, size, price, 2, 50.0);
+                    case 8:
+                    case 9:
+                    case 0:
+                        floors = 3;
+                        price = 4800000.0;
+                        size = 80.0;
+                        return new Detached(blockNum, lotNum, floors, size, price, 2, 50.0);
+                    default:
+                        return null;
+                }
             default:
                 return null;
         }
